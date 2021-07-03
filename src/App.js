ef2e94496b1ed83a67586da6d12d2ac1a00e9a0e
chien <chiennguyen.btw@gmail.com>
2021-07-03 12:31:07 +0700

    Fifth Commit
    Convert Class component to Function component

--- src/App.js
+++ src/App.js
@@ -8,23 +8,22 @@ function App(props) {
   const items1 = ["Hoa", "Huong", "Ha", "Chien"];
   const items2 = ["Ronaldo", "Messi", "Neymar", "Chien"];
 
-  const[itemSelected, setItemSelected] = React.useState(null);
+  // const[itemSelected, setItemSelected] = React.useState(null);
   // const handleChangeSelector = React.useCallback((selectedItem) => {
   //   console.log("handleChangeSelector", selectedItem);
   //   alert(selectedItem);
   // }, []);
 
-  // cách dùng useState
-  const handleChangeSelector = React.useCallback((value) => {
-    setItemSelected(value);
-  }, []);
+  // // cách dùng useState
+  // const handleChangeSelector = React.useCallback((value) => {
+  //   setItemSelected(value);
+  // }, []);
 
   return (
     <div className="app">
-      <Selector mode="single" onSelected={handleChangeSelector} list={items1} />
-      <Selector mode="single" onSelected={handleChangeSelector} list={items2} />
+      <Selector/>
       <hr/>
-      {itemSelected}
+      {/* {itemSelected} */}
     </div>
   );
 }
--- src/component/Selector.js
+++ src/component/Selector.js
@@ -1,32 +1,27 @@
 import React from "react";
 
-class Selector extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { items: this.props?.list ? this.props.list : [] };
-    this.changeItems = this.changeItems.bind(this);
-  }
+function Selector(props) {
+  const [isSingle, setSingle] = React.useState(true);
+  const [items, setItems] = React.useState(["Chien", "Viet", "Dung"]);
 
-  changeItems(event) {
+  function changeItems(event) {
     console.log("changeItems: ", event.target.value);
-    this.props.onSelected(event.target.value);
+    return event.target.value;
   }
 
-  render() {
-    return (
-      <>
-        {this.state?.items && this.state.items?.length > 0 && (
-          <select onChange={this.changeItems}>
-            {this.state.items.map((item, index) => (
-              <option key={index} value={item}>
-                {item}
-              </option>
-            ))}
-          </select>
-        )}
-      </>
-    );
-  }
+  return (
+    <>
+      {items && items?.length > 0 && (
+        <select onChange={changeItems}>
+          {items.map((item, index) => (
+            <option key={index} value={item}>
+              {item}
+            </option>
+          ))}
+        </select>
+      )}
+    </>
+  );
 }
 
 export default Selector;
